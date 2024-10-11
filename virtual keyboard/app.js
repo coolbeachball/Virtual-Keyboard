06750027153fac4544463ed1bcb47a19b0b352a9
CoolBeachBall <coolbeachball@proton.me>
2024-10-11 14:54:44 +0300

    Add files via upload

---
+++ virtual keyboard/app.js
@@ -0,0 +1,7 @@
+const keys = document.getElementsByClassName('.key');
+const backSpaceKey = document.getElementsByClassName('.back-space-key');
+const tabKey = document.getElementsByClassName('.tab-key');
+const capsLockKey = document.getElementsByClassName('.caps-lock-key');
+const enterKey = document.getElementsByClassName('.enter-key');
+const leftShiftKey = document.getElementsByClassName('.left-shift-key');
+const rightShiftKey = document.getElementsByClassName('.right-shift-key');
